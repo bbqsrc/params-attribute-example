9b01cda78fd2f802fb6d0c6397ffd4f2e284721c
Hexilee <hexileee@gmail.com>
2019-08-02 12:31:54 +0800

    remove unneccessary dependencies in master; get a minimal repo

--- proc-macro-tests/rename_params.rs
+++ proc-macro-tests/rename_params.rs
@@ -2,6 +2,4 @@
 use params_attribute_example::rename_params;
 
 #[rename_params]
-fn hello(a: i32, #[val] b: i32) {
-
-}
+fn hello(a: i32, b: i32) {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,8 @@
-#![feature(proc_macro_diagnostic)]
-
 extern crate proc_macro;
-use proc_macro::{Diagnostic, Level, TokenStream};
-use quote::quote;
-use syn::{
-    parse::{Parse, Parser},
-    FnArg, ItemFn, Pat,
-};
+use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
 pub fn rename_params(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let func = <ItemFn as Parse>::parse.parse(input).unwrap();
-    for arg in func.sig.inputs.iter() {
-        if let FnArg::Typed(pat) = arg {
-            if let Pat::Ident(name) = pat.pat.as_ref() {
-                Diagnostic::new(
-                    Level::Note,
-                    format!(
-                        "arg({}), PatType.attrs.len = {}, PatIdent.attrs.len = {}",
-                        name.ident,
-                        pat.attrs.len(),
-                        name.attrs.len()
-                    ),
-                )
-                .emit();
-            }
-        }
-    }
-    quote!(#func).into()
+    println!("{}", &input);
+    input
 }
